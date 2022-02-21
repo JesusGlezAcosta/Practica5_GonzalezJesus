1c7cdd0a0cadedda6081c715eba446a2f1ea9afb
jesus <jesusg.gonzalez@potros.itson.edu.mx>
2022-02-21 02:25:22 -0800

    Initial commit

---
+++ app/src/main/java/gonzalez/jesus/thecheezery/RegisterActivity.kt
@@ -0,0 +1,20 @@
+package gonzalez.jesus.thecheezery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val button: Button = findViewById(R.id.sign_ind_button)
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
